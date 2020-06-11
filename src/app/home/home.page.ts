00dac3c6c4efbe7643a17610de1400034a007265
Pascal Graf <pascal.graf@gmx.net>
2020-06-11 16:03:13 +0200

    add action sheet button for bug presentation

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {ActionSheetController} from '@ionic/angular';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+    selector: 'app-home',
+    templateUrl: 'home.page.html',
+    styleUrls: ['home.page.scss'],
 })
 export class HomePage {
 
-  constructor() {}
+    constructor(private actionSheetController: ActionSheetController) {
+    }
 
+    async presentActionSheet(): Promise<void> {
+        const sheet = await this.actionSheetController.create({
+            buttons: [
+                {
+                    text: 'Action',
+                    handler: () => {
+                        console.log('handler triggered');
+                    }
+                },
+                {
+                    text: 'Cancel',
+                    role: 'cancel'
+                }
+            ]
+        });
+        await sheet.present();
+    }
 }
