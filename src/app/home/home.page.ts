1dcf722de9277d687e4577013d569f55d884b6c9
Pascal Graf <pascal.graf@gmx.net>
2020-06-12 16:12:26 +0200

    add list for icon click error

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -8,7 +8,17 @@ import {ActionSheetController} from '@ionic/angular';
 })
 export class HomePage {
 
+    public form = [
+        {icon: 'boat-outline', val: 'Boat', isChecked: true},
+        {icon: 'bug-outline', val: 'Bug', isChecked: false},
+        {icon: 'car-outline', val: 'Car', isChecked: false}
+    ];
+
     constructor(private actionSheetController: ActionSheetController) {
+    }
+
+    itemClickHandler(): void {
+        console.log('item clicked');
     }
 
     async presentActionSheet(): Promise<void> {
